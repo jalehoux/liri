dbdb645575fc398e862d5df1d64c029e96a8b1c3
jalehoux <jalehoux@gmail.com>
2018-08-19 20:45:23 -0400

    added readme and bonus

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# lirl
+# LIRI - Node App
+
+A terminal shell node app where you can query and return data for movies, bands, and song info from Spotify
+
+## Getting Started
+
+Clone or download this repository to make use of the command line interface.  A seperate ENV file will have to be added for your own uses.  Node is also required for this.
+
+### How to use
+
+The four commands to use this app are below. 
+
+* concert-this "band name here"
+* spotify-this-song "song name here"
+* movie-this "movie name here'
+* do-what-it-says
+
+```
+node index concert-this '3 doors down'
+```
+
+## Built With
+
+* [Spotify API](https://developer.spotify.com/documentation/web-api/)
+* [Node](https://nodejs.org/en/)
+* [Request NPM](https://www.npmjs.com/package/request)
+* [Bands in town API](http://www.artists.bandsintown.com/bandsintown-api)
+
+
+## Authors
+
+* **Jacob Lehoux** - *Initial work* - [jalehoux](https://github.com/jalehoux)
+
+See also my personal website [here](https://jacoblehoux.com/)
+
+## Acknowledgments
+
+* UNH Coding Bootcamp
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const spotify = new Spotify(keys.spotify);
 
 let command = process.argv[2];
 
-switch(command) {
+switch (command) {
     case "concert-this":
         getConcert(process.argv[3]);
         break;
@@ -30,7 +30,7 @@ switch(command) {
 }
 
 function getConcert(band) {
-    request(`https://rest.bandsintown.com/artists/${band}/events?app_id=codingbootcamp`,function(err,res,body){
+    request(`https://rest.bandsintown.com/artists/${band}/events?app_id=codingbootcamp`, function (err, res, body) {
         var arr = JSON.parse(body)
         arr.forEach(element => {
             console.log(`${element.venue.name} - ${element.venue.city}, ${element.venue.region} on ${element.datetime}`)
@@ -39,34 +39,72 @@ function getConcert(band) {
 }
 
 function getSpotify(song) {
-    spotify.search({ type: 'track', query: song}, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-        var items = data.tracks.items;
-        items.forEach(element => {
-            console.log(`${element.name} - ${element.artists[0].name}`);
-            console.log(`${element.album.name} - ${element.preview_url}`);
-            console.log("------------------------------------");          
-        });
-    })    
+    if (song) {
+        spotify.search({ type: 'track', query: song }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            var items = data.tracks.items;
+            items.forEach(element => {
+                console.log(`${element.name} - ${element.artists[0].name}`);
+                console.log(`${element.album.name} - ${element.preview_url}`);
+                console.log("------------------------------------");
+            });
+        })
+    } else {
+        spotify.search({ type: 'track', query: 'The Sign' }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            var items = data.tracks.items;
+            items.forEach(element => {
+                console.log(`${element.name} - ${element.artists[0].name}`);
+                console.log(`${element.album.name} - ${element.preview_url}`);
+                console.log("------------------------------------");
+            });
+        })
+    }
 }
 
-function getMovie(movie){
-    request(`http://www.omdbapi.com/?apikey=trilogy&t=${movie}`,function(err,res,body){
-        var data = JSON.parse(body);
-        console.log(`${data.Title} - ${data.Year}`);
-        console.log(`${data.Ratings[0].Source} - ${data.Ratings[0].Value}`);
-        console.log(`${data.Ratings[1].Source} - ${data.Ratings[1].Value}`);
-        console.log(`${data.Country} - ${data.Language}`)
-        console.log(`Plot: ${data.Plot}`);
-        console.log(`Actors: ${data.Actors}`);
-        console.log('--------------------------------------------')
-    })
+function getMovie(movie) {
+    if (movie) {
+        request(`http://www.omdbapi.com/?apikey=trilogy&t=${movie}`, function (err, res, body) {
+            var data = JSON.parse(body);
+            console.log(`${data.Title} - ${data.Year}`);
+            console.log(`${data.Ratings[0].Source} - ${data.Ratings[0].Value}`);
+            console.log(`${data.Ratings[1].Source} - ${data.Ratings[1].Value}`);
+            console.log(`${data.Country} - ${data.Language}`)
+            console.log(`Plot: ${data.Plot}`);
+            console.log(`Actors: ${data.Actors}`);
+            console.log('--------------------------------------------')
+
+            fs.appendFile('log.txt', `Movie searched.  Title: ${data.Title}`, function (err) {
+                if (err) throw err;
+                console.log('Search Logged!');
+            });
+        })
+    } else {
+        var fallBack = "Mr. Nobody";
+        request(`http://www.omdbapi.com/?apikey=trilogy&t=${fallBack}`, function (err, res, body) {
+            var data = JSON.parse(body);
+            console.log(`${data.Title} - ${data.Year}`);
+            console.log(`${data.Ratings[0].Source} - ${data.Ratings[0].Value}`);
+            console.log(`${data.Ratings[1].Source} - ${data.Ratings[1].Value}`);
+            console.log(`${data.Country} - ${data.Language}`)
+            console.log(`Plot: ${data.Plot}`);
+            console.log(`Actors: ${data.Actors}`);
+            console.log('--------------------------------------------')
+
+            fs.appendFile('log.txt', `Movie searched.  Title: ${data.Title}`, function (err) {
+                if (err) throw err;
+                console.log('Search Logged!');
+            });
+        })
+    }
 }
 
 function getDoIt() {
-    fs.readFile("random.txt", "utf8", function(error, data){
+    fs.readFile("random.txt", "utf8", function (error, data) {
         if (!error) {
             getSpotify(data);
         } else {
